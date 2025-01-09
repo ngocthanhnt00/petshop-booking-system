6ad9c64a35251900075702ba1f363ba8fc2786cd
ngocthanhnt00 <thanhnnps36626@fpt.edu.vn>
2025-01-09 13:47:45 +0700

    feat: update env and app file

--- backend/config.js
+++ backend/config.js
@@ -6,6 +6,7 @@ const ENV_VARS = {
   MONGODB_URI: process.env.MONGODB_URI,
   JWT_SECRET: process.env.JWT_SECRET,
   NODE_ENV: process.env.NODE_ENV,
+  API_URL: process.env.API_URL,
 };
 
 export default ENV_VARS;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
+import logger from "morgan";
 import { protectRoute } from "./middleware/protectRoute.js";
-import dotenv from "dotenv";
 
 import ENV_VARS from "./config.js";
 const app = express();
@@ -12,10 +12,17 @@ const PORT = ENV_VARS.PORT;
 import authRouter from "./routes/auth.route.js";
 import indexRouter from "./routes/index.route.js";
 import { connectDB } from "./db.js";
-dotenv.config();
-
+const corsOptions = {
+  origin: `${ENV_VARS.API_URL}`,
+  credentials: true, // This allows the server to accept cookies from the client
+};
+app.use(cors(corsOptions));
+console.log(ENV_VARS.API_URL);
 app.use(express.json());
 app.use(cookieParser());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 console.log("MONGO_URI", process.env.MONGODB_URI);
 app.use("/api/v1/auth", authRouter);
 app.use("/", indexRouter);
