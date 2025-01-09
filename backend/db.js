18cc68dc17c1bf274e94c81aa9a3054f05e6ba00
ngocthanhnt00 <thanhnnps36626@fpt.edu.vn>
2025-01-09 13:55:50 +0700

    feat: update db.js file

--- backend/db.js
+++ backend/db.js
@@ -4,9 +4,9 @@ import ENV_VARS from "./config.js";
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(ENV_VARS.MONGODB_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`Kết nối thành công: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error connecting to MongoDB: ${error.message}`);
+    console.error(`Kết nối thất bại: ${error.message}`);
     process.exit(1); // 1 means exist with failure, 0 means exist with success
   }
 };
